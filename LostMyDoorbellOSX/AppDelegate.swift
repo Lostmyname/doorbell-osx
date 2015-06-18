481d2d07762b4996819b6e1d080f2ec16e1424de
Simon Fry <simonpfry@simonflostmyname.local>
2015-06-18 17:52:33 +0100

    Initial Commit

---
+++ LostMyDoorbellOSX/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LostMyDoorbellOSX
+//
+//  Created by Simon Fry on 18/06/2015.
+//  Copyright (c) 2015 LostMy.Name. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
